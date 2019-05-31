a18419eebfc2f052b5a5d8c52982f4a657123d06
Ryu <ryujt658@hanmail.net>
2019-05-31 14:49:17 +0900

    초기 커밋.

---
+++ ClassTest/Program.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClassTest
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            /*
+            Rectangle rect1;
+            rect1.length = 100;
+            rect1.width = 30;
+
+            Console.WriteLine("rectangle length:{0}, width:{1}", rect1.length, rect1.width);
+
+            Rectangle rect2 = new Rectangle(200, 50);
+            Console.WriteLine("rectangle length:{0}, width:{1}, area:{2}",
+                rect1.length, rect1.width, rect1.Area());
+            */
+
+            Animal fox = new Animal("fox", "Raaww");
+            fox.MakeSound();
+
+            Animal dog = new Animal("dog", "Walwal");
+            dog.MakeSound();
+
+            Animal cat = new Animal("cat", "nyaong");
+            cat.MakeSound();
+
+            Console.ReadLine();
+        }
+
+        struct Rectangle
+        {
+            public double length;
+            public double width;
+
+            public Rectangle(double l, double w)
+            {
+                length = l;
+                width = w;
+            }
+
+            public double Area()
+            {
+                return length * width;
+            }
+        }
+    }
+}
